dbdc13dc79b1471618ad6cca5a5349043fe3002d
Aakash Patel <akashpatelp83@gmail.com>
2023-09-07 03:22:01 +0530

    Update Controller for modeled data request response

--- VegasBoost/Controllers/BreakfastController.cs
+++ VegasBoost/Controllers/BreakfastController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using VegasBoost.Contracts.Vegas;
+using VegasBoost.Models;
 
 namespace VegasBoost.Controllers
 {
@@ -10,7 +11,31 @@ namespace VegasBoost.Controllers
         [HttpPost("")]
         public IActionResult CreateBreakfast(CreateBreakfastRequest request)
         {
-            return Ok(request);
+            var breakfast = new Breakfast(
+                Guid.NewGuid(),
+                request.Name,
+                request.Description,
+                request.StartDateTime,
+                request.EndDateTime,
+                DateTime.UtcNow,
+                request.Savory,
+                request.Sweet);
+            //ToDO: Save nreakfast to database
+
+            var response = new BreakfastResponse(
+                breakfast.Id,
+                breakfast.Name,
+                breakfast.Description,
+                breakfast.EndDateTime,
+                breakfast.StartDateTime,
+                breakfast.LastModifiedDateTime,
+                breakfast.Savory,
+                breakfast.Sweet);
+                
+            return CreatedAtAction(
+                actionName: nameof(GetBreakfast),
+                routeValues: new {id = breakfast.Id},
+                value: response);
         }
 
 
