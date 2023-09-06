83feecefeb48ebe5b253907f6ca975f4ef86cc10
Aakash Patel <akashpatelp83@gmail.com>
2023-09-07 02:20:21 +0530

    add default routing for BreakfastController

--- VegasBoost/Controllers/BreakfastController.cs
+++ VegasBoost/Controllers/BreakfastController.cs
@@ -4,30 +4,31 @@ using VegasBoost.Contracts.Vegas;
 namespace VegasBoost.Controllers
 {
     [ApiController]
+    [Route("[controller]")]
     public class BreakfastController : ControllerBase
     {
-        [HttpPost("/breakfast")]
+        [HttpPost("")]
         public IActionResult CreateBreakfast(CreateBreakfastRequest request)
         {
             return Ok(request);
         }
 
 
-        [HttpGet("/breakfast/{id:guid}")]
+        [HttpGet("{id:guid}")]
         public IActionResult GetBreakfast(Guid id)
         {
             return Ok(id);
         }
 
 
-        [HttpPut("/breakfast{id:Guid}")]
+        [HttpPut("{id:guid}")]
         public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
         {
             return Ok(request);
         }
 
 
-        [HttpDelete("/breakfast/{id:guid}")]
+        [HttpDelete("{id:guid}")]
         public IActionResult DeleteBreakfast(Guid id)
         {
             return Ok(id);
