9a41244b59d9d5ea97dd2c08b461605d9544ec15
Aakash Patel <akashpatelp83@gmail.com>
2023-09-07 01:41:56 +0530

    Build Metadata

--- VegasBoost/Program.cs
+++ VegasBoost/Program.cs
@@ -1,11 +1,12 @@
-
+// For dependecy injection and configuration 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 {
+    // Add services to the container.
     builder.Services.AddControllers();
 } 
 
+// Manage Request Pipeline
 var app = builder.Build();
 {
     app.UseHttpsRedirection();
